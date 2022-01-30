ccd848e9114ab88e310953768ed462d545c8ff6d
NEONz <neonzdeveloper@gmail.com>
2022-01-30 13:54:37 +0100

    Installer Updated

--- installer.py
+++ installer.py
@@ -12,12 +12,7 @@ time.sleep(1)
 
 try:
     print('Installing Dependencies...')
-    os.system('pip3 install py-cpuinfo')
-except:
-    print("Error Ocurred, maybe you don't have pip3 installed.")
-    time.sleep(1)
-try:
-    os.system('sudo apt update -y && sudo apt install python3 -y && sudo apt install python3-pip')
+    os.system('sudo apt update -y && sudo apt install python3 -y && sudo apt install python3-pip && pip3 install py-cpuinfo')
 except:
     print('Uknown error')
 
