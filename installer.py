91b0f35398022927f486afb8ece51eebd30ecfb1
NEONz <neonzdeveloper@gmail.com>
2022-01-30 13:48:35 +0100

    Installer Updated (beta)

--- installer.py
+++ installer.py
@@ -10,7 +10,13 @@ print('''
 
 time.sleep(1)
 
-print('Installing Dependencies...')
-os.system('pip3 install py-cpuinfo')
-print('All Dependencies Installed Successfully')
-
+try:
+    print('Installing Dependencies...')
+    os.system('pip3 install py-cpuinfo')
+    print('All Dependencies Installed Successfully')
+except:
+    print("Error Ocurred, maybe you don't have pytho3 or pip3 installed.")
+try:
+    os.system('sudo apt install python3 -y && sudo apt install python3-pip')
+except:
+    print('Uknown error')
---
+++ test.py
@@ -0,0 +1,6 @@
+import re
+import cpuinfo
+
+result = cpuinfo.get_cpu_info()
+
+print(result)
