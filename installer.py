7a384f8b37e1eeb210cb2b47ef1630ed8c76c5f1
NEONz <neonzdeveloper@gmail.com>
2022-01-30 14:12:43 +0100

    Installer Updated (Experimental)

--- installer.py
+++ installer.py
@@ -1,5 +1,6 @@
 import os
 import time
+import platform
 print('''
 ██╗███╗░░██╗░██████╗████████╗░█████╗░██╗░░░░░██╗░░░░░███████╗██████╗░
 ██║████╗░██║██╔════╝╚══██╔══╝██╔══██╗██║░░░░░██║░░░░░██╔════╝██╔══██╗
@@ -9,11 +10,16 @@ print('''
 ╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚══════╝╚══════╝╚═╝░░╚═╝''')
 
 time.sleep(1)
+if platform.system() == 'Linux':
+    try:
+        print('Installing Dependencies...')
+        os.system('sudo apt update -y && sudo apt install python3 -y && sudo apt install python3-pip && pip3 install py-cpuinfo')
+        print('All Dependencies Installed Successfully')
+    except:
+        print('Uknown error')
 
-try:
-    print('Installing Dependencies...')
-    os.system('sudo apt update -y && sudo apt install python3 -y && sudo apt install python3-pip && pip3 install py-cpuinfo')
-except:
-    print('Uknown error')
 
-print('All Dependencies Installed Successfully')
+if platform.system() == 'Windows':
+    os.system('pip3 install py-cpuinfo')
+    print('All Dependencies Installed Successfully')
+
