c622f01bdc1a9502526403b69e8fe736cae87f3b
NEONz <neonzdeveloper@gmail.com>
2022-01-30 14:18:32 +0100

    Installer Updated (Experimental)

--- installer.py
+++ installer.py
@@ -15,6 +15,7 @@ if platform.system() == 'Linux':
         print('Installing Dependencies...')
         os.system('sudo apt update -y && sudo apt install python3 -y && sudo apt install python3-pip && pip3 install py-cpuinfo')
         print('All Dependencies Installed Successfully')
+        print('Use python3 index.py')
     except:
         print('Unknown error')
 
@@ -23,5 +24,6 @@ if platform.system() == 'Windows':
     try:
         os.system('pip3 install py-cpuinfo')
         print('All Dependencies Installed Successfully')
+        print('Use python3 index.py')
     except:
         print('Unknown Error')
